35b34a3bac94e55f0bc476d9cd18903f384f209c
Xu Liang <liang.xu@hotstar.com>
2022-10-07 23:33:21 +0800

    fix message

--- db/message.go
+++ db/message.go
@@ -14,7 +14,7 @@ func (m *message) Create(message *model.Message) error {
 
 func (m *message) ListLatest(offset, size int) ([]model.Message, error) {
 	var messages []model.Message
-	err := m.db.Preload(clause.Associations).Model(&model.Message{}).Order(clause.OrderByColumn{Column: clause.Column{Name: "created_at"}, Desc: true}).Offset(offset).Limit(size).Find(&messages).Error
+	err := m.db.Preload(clause.Associations).Order(clause.OrderByColumn{Column: clause.Column{Name: "created_at"}, Desc: true}).Offset(offset).Limit(size).Find(&messages).Error
 	if err == gorm.ErrRecordNotFound {
 		err = nil
 	}
